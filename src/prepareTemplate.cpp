3389e23aaa7baab79fc0bc0206fa1cc8d6ffae47
Yutarotaro <tengyeyu@gmail.com>
2020-12-21 14:06:14 +0900

    pose estimation for bthesis on linux

--- src/prepareTemplate.cpp
+++ src/prepareTemplate.cpp
@@ -1,4 +1,3 @@
-#include "Eigen/Dense"
 #include <cstdlib>
 #include <fstream>
 #include <iomanip>
@@ -10,67 +9,78 @@
 cv::Mat temp;
 int meter_type;
 std::string meter_type_s;
-//type 0: normal, 1: pointer_considered
+// type 0: normal, 1: pointer_considered
 int type;
-//record 0: no, 1:Yes
+// record 0: no, 1:Yes
 int record;
 
 void message();
 
-
-class Params
-{
+class Params {
 public:
-    cv::Point center;
-    int radius;
-    std::string picdir;  //ディレクトリ
-    std::string index;
-    cv::Point tl;  //左上
-    int l;         //矩形の大きさ
+  cv::Point center;
+  int radius;
+  std::string picdir; //ディレクトリ
+  std::string index;
+  cv::Point tl; //左上
+  int l;        //矩形の大きさ
 };
 
-//0:T, 1:V
-Params params[] = {{cv::Point(2214, 1294), 310, "meter_experiment", "-3", cv::Point(1899, 979), 620},
-    {cv::Point(2158, 1383), 340, "meter_experiment_V", "-1", cv::Point(1808, 1033), 680},
-    {cv::Point(2160, 1708), 372, "dia_experiment_V", "-1", cv::Point(1762, 1315), 784}};
+// 0:T, 1:V
+Params params[] = {
+    {cv::Point(2214, 1294), 310, "meter_experiment", "-3", cv::Point(1899, 979),
+     620},
+    {cv::Point(2158, 1383), 340, "meter_experiment_V", "-1",
+     cv::Point(1808, 1033), 680},
+    {cv::Point(2160, 1708), 372, "dia_experiment_V", "-1",
+     cv::Point(1762, 1315), 784},
+    {cv::Point(2109, 1616), 243, "bthesis", "-2", cv::Point(1864, 1374), 486},
+    {cv::Point(2263, 1800), 316, "bthesis", "-1", cv::Point(1947, 1482), 632}};
 
 std::map<std::string, int> mp;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
 
-    message();
+  message();
 
-    cv::Mat Base_clock_tmp = cv::imread("../pictures/" + params[meter_type].picdir + "/pic" + params[meter_type].index + ".JPG", 1);
+  cv::Mat Base_clock_tmp =
+      cv::imread("../pictures/" + params[meter_type].picdir + "/pic" +
+                     params[meter_type].index + ".JPG",
+                 1);
 
+  // 2158,1383
+  //文字盤以外にマスクをかける処理
+  cv::Mat mask =
+      cv::Mat::zeros(Base_clock_tmp.rows, Base_clock_tmp.cols, CV_8UC1);
+  cv::circle(mask, params[meter_type].center, params[meter_type].radius,
+             cv::Scalar(255), -1, CV_AA);
 
-    //2158,1383
-    //文字盤以外にマスクをかける処理
-    cv::Mat mask = cv::Mat::zeros(Base_clock_tmp.rows, Base_clock_tmp.cols, CV_8UC1);
-    cv::circle(mask, params[meter_type].center, params[meter_type].radius, cv::Scalar(255), -1, CV_AA);
+  cv::Mat Base_clock; //メータ領域だけを残した基準画像
+  Base_clock_tmp.copyTo(Base_clock, mask);
 
-    cv::Mat Base_clock;  //メータ領域だけを残した基準画像
-    Base_clock_tmp.copyTo(Base_clock, mask);
+  cv::Rect roi_temp(
+      params[meter_type].tl,
+      cv::Size(params[meter_type].l,
+               params[meter_type].l)); //基準画像におけるメータ文字盤部分の位置
+  temp = Base_clock(roi_temp);
 
-    cv::Rect roi_temp(params[meter_type].tl, cv::Size(params[meter_type].l, params[meter_type].l));  //基準画像におけるメータ文字盤部分の位置
-    temp = Base_clock(roi_temp);
+  ///////////////////////////////
 
-    ///////////////////////////////
-
-    cv::imwrite("../pictures/meter_template/Base_clock" + meter_type_s + ".png", Base_clock);
-    cv::imwrite("../pictures/meter_template/temp" + meter_type_s + ".png", temp);
+  cv::imwrite("../pictures/meter_template/Base_clock" + meter_type_s + ".png",
+              Base_clock);
+  cv::imwrite("../pictures/meter_template/temp" + meter_type_s + ".png", temp);
 }
 
+void message() {
+  mp["T"] = 0;
+  mp["V"] = 1;
+  mp["dia_V"] = 2;
+  mp["bthesis"] = 4;
 
-void message()
-{
-    mp["T"] = 0;
-    mp["V"] = 1;
-    mp["dia_V"] = 2;
+  std::cout << "type of analog meter: ThermoMeter -> T or Vacuum -> V or dia_V "
+               "-> dia_V"
+            << std::endl;
+  std::cin >> meter_type_s;
 
-    std::cout << "type of analog meter: ThermoMeter -> T or Vacuum -> V or dia_V -> dia_V" << std::endl;
-    std::cin >> meter_type_s;
-
-
-    meter_type = mp[meter_type_s];
+  meter_type = mp[meter_type_s];
 }
--- src/test.cpp
+++ src/test.cpp
@@ -18,7 +18,7 @@ Camera_pose camera;
 int type;
 
 cv::Mat temp;
-std::string meter_type_s = "dia_V";
+std::string meter_type_s = "bthesis";
 int it;
 int meter_type = 2;
 
@@ -35,153 +35,166 @@ cv::Mat HP;
 Init::Params params;
 
 cv::Point ROI_tl[1000];
-int main(int argc, char** argv)
-{
-    //parameter
-    Init::parseInit();
+int main(int argc, char **argv) {
+  std::cout << std::endl << argv[1] << "-th image" << std::endl;
+  // parameter
+  Init::parseInit();
 
-    // JSONデータの読み込み。
-    std::ifstream ifs("save.json", std::ios::in);
-    if (ifs.fail()) {
-        std::cerr << "failed to read test.json" << std::endl;
-        return 1;
+  // JSONデータの読み込み。
+  std::ifstream ifs("bthesis_roi.json", std::ios::in);
+  if (ifs.fail()) {
+    std::cerr << "failed to read test.json" << std::endl;
+    return 1;
+  }
+  const std::string json((std::istreambuf_iterator<char>(ifs)),
+                         std::istreambuf_iterator<char>());
+  ifs.close();
+
+  // JSONデータの読み込み
+  picojson::value v;
+  const std::string err = picojson::parse(v, json);
+  if (err.empty() == false) {
+    std::cerr << err << std::endl;
+    return 2;
+  }
+  picojson::object &obj = v.get<picojson::object>();
+
+  cv::Mat Base_calib = cv::imread("../pictures/bthesis/pic-1.JPG", 1);
+  cv::Mat Base_clock =
+      cv::imread("../pictures/meter_template/Base_clockbthesis.png", 1);
+  //   cv::imread("../pictures/meter_template/Base_clockV.png", 1);
+  //     cv::imread("../pictures/meter_template/Base_clockdia_V.png", 1);
+
+  // Base_clockの特徴点を保存
+  Init::Feature Base(Base_clock);
+  // Init::Feature Base("../pictures/meter_template/Base_clockdia_V.png");
+
+  Module::pose p;
+  Calib::calibration(Base_calib, p,
+                     1); // pにテンプレート画像のカメラ視点，位置が入る
+  // image上のchessboardからtemplateのカメラの位置姿勢を推定
+
+  cv::Mat rvec; //回転ベクトル
+
+  int st = std::stoi(argv[1]);
+  int to = st;
+
+  //読み取り結果を記録
+  std::string fileName = "./diffjust/" + meter_type_s + "/data/pos/" +
+                         (argc >= 3 ? argv[2] : "") + ".csv";
+  std::ofstream ofs(fileName, std::ios::app);
+
+  int ct = 0;
+
+  for (int i = st; i <= to; i++) {
+
+    picojson::array &ary = obj[std::to_string(i)].get<picojson::array>();
+    ROI_tl[i].x = std::stoi(ary[0].get<std::string>());
+    ROI_tl[i].y = std::stoi(ary[1].get<std::string>());
+
+    std::string path_calib =
+        "../pictures/" + meter_type_s + "/pic" + std::to_string(i) + ".JPG";
+
+    // mask image
+    std::string path_clock = "../pictures/" + meter_type_s + "/detection/pic" +
+                             std::to_string(i) + ".png";
+
+    Init::Feature Now_calib(path_calib); // for ground truth
+    Init::Feature Now_clock(path_clock); // for homography decomposition
+
+    Module::pose q;
+    Calib::calibration(Now_calib.img, q,
+                       0); // qにテスト画像のカメラ視点，位置が入る
+
+    cv::Mat R_calib = q.orientation * p.orientation.inv();
+    cv::Rodrigues(R_calib, rvec);
+
+    // offset world coordinate
+    // cv::Mat offset = (cv::Mat_<double>(3, 1) << -337., -73.2, -410.); //for
+    // dia
+    cv::Mat offset = (cv::Mat_<double>(3, 1) << 446.9431995993519,
+                      79.59721708740386, 332.); // for bthesis
+
+    //横軸: メータ平面上に決めた原点からの距離(chess boardからではない)
+    cv::Mat distance = q.orientation.inv() * q.position + offset;
+
+    std::cout << "dist. from meter of Test Image: " << -distance << std::endl;
+
+    // pose estimation誤差
+    cv::Mat calib_pos =
+        p.orientation.inv() * p.position - q.orientation.inv() * q.position;
+
+    std::cout << "chessboard並進ベクトル" << std::endl
+              << calib_pos << std::endl
+              << "回転ベクトル" << std::endl
+              << rvec << std::endl
+              << std::endl;
+
+    ofs << i << ',' << cv::norm(distance) << ','
+        << cv::norm(rvec) * 180.0 / CV_PI << ',';
+    //先にgroundtruthの方は保存しておく
+    // Homgraphy Estimation
+
+    Init::Feature ROI("../pictures/" + meter_type_s + "/roi/pic" +
+                      std::to_string(i) + ".png");
+    cv::Mat roi = cv::imread("../pictures/" + meter_type_s + "/roi/pic" +
+                                 std::to_string(i) + ".png",
+                             1);
+    // double temp_size = 784.;
+    double temp_size = 632.;
+    double rate = std::max(temp_size / roi.cols, temp_size / roi.rows);
+
+    cv::Mat resized_roi;
+    cv::resize(roi, resized_roi, cv::Size(), rate, rate);
+    Init::Feature R_ROI(resized_roi);
+
+    Base.getHomography(R_ROI);
+    for (auto j : R_ROI.featurePoint) {
+      ROI.featurePoint.push_back(
+          cv::Point2f(j.x / rate + ROI_tl[i].x, j.y / rate + ROI_tl[i].y));
+      // resized_roi内座標をNow_clock内座標に変換
     }
-    const std::string json((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-    ifs.close();
 
-    // JSONデータを解析する。
-    picojson::value v;
-    const std::string err = picojson::parse(v, json);
-    if (err.empty() == false) {
-        std::cerr << err << std::endl;
-        return 2;
-    }
-    picojson::object& obj = v.get<picojson::object>();
+    cv::Mat masks;
+    cv::Mat H4 = cv::findHomography(Base.featurePoint, ROI.featurePoint, masks,
+                                    cv::RANSAC, 3);
 
+    cv::Mat warped_2 =
+        cv::Mat::zeros(Now_clock.img.rows, Now_clock.img.cols, CV_8UC3);
+    cv::warpPerspective(Now_clock.img, warped_2, H4.inv(), warped_2.size());
 
-    // cv::Mat Base_calib = cv::imread("../pictures/meter_experiment/pic-1.JPG", 1);
-    cv::Mat Base_calib = cv::imread("../pictures/dia_experiment_V/pic0.JPG", 1);
-    //cv::Mat Base_clock_tmp = cv::imread("../pictures/meter_experiment/pic-2.JPG", 1);
-    cv::Mat Base_clock = cv::imread("../pictures/meter_template/Base_clockdia_V.png", 1);
+    Init::Feature Warp(warped_2);
+    cv::Mat H3 = Base.getHomography(Warp);
 
+    Module::pose r = Module::decomposeHomography(H4 * H3, camera.A);
 
-    //Base_clockの特徴点を保存
-    Init::Feature Base(Base_clock);
-    //Init::Feature Base("../pictures/meter_template/Base_clockdia_V.png");
+    std::cout << std::endl
+              << "Homography分解で得られた並進ベクトル(world)" << std::endl
+              << r.position << std::endl
+              << std::endl
+              << "回転ベクトル" << std::endl
+              << r.orientation << std::endl
+              << std::endl;
 
+    cv::Mat R_estimated;
+    cv::Rodrigues(r.orientation, R_estimated);
 
-    Module::pose p;
-    Calib::calibration(Base_calib, p, 1);  //pに基準とする画像のカメラ視点，位置が入る
-    //image上のchessboardからtemplateのカメラの位置姿勢を推定
+    cv::Mat angle_error_mat = R_estimated * R_calib.inv();
+    cv::Mat angle_error;
+    cv::Rodrigues(angle_error_mat, angle_error);
+    double error = cv::norm(angle_error);
 
-    cv::Mat rvec;  //回転ベクトル
+    std::cout << "distance to meter (mm) = " << cv::norm(distance) << std::endl
+              << "error dist(mm) = " << cv::norm(calib_pos - r.position)
+              << std::endl
+              << cv::norm(r.orientation) * 180.0 / CV_PI << ' '
+              << cv::norm(rvec) * 180.0 / CV_PI << ' ' << error * 180.0 / CV_PI
+              << std::endl;
 
-    int st = std::stoi(argv[1]);
-    int to = st;
-
-
-    //読み取り結果を記録
-    std::string fileName = "./diffjust/" + meter_type_s + "/data/pos/" + (argc >= 3 ? argv[2] : "") + ".csv";
-    std::ofstream ofs(fileName, std::ios::app);
-
-    int ct = 0;
-
-
-    for (int i = st; i <= to; i++) {
-        std::cout << i << "-th image" << std::endl;
-
-
-        picojson::array& ary = obj[std::to_string(i)].get<picojson::array>();
-        ROI_tl[i].x = std::stoi(ary[0].get<std::string>());
-        ROI_tl[i].y = std::stoi(ary[1].get<std::string>());
-
-
-        std::string path_calib = "../pictures/dia_experiment_V/pic" + std::to_string(i) + ".JPG";
-
-        //mask image
-        std::string path_clock = "../pictures/dia_experiment_V/mask/pic" + std::to_string(i) + ".png";
-
-        cv::Mat Now_calib = cv::imread(path_calib, 1);
-        // cv::Mat Now_clock = cv::imread(path_clock, 1);
-        Init::Feature Now_clock(path_clock);
-
-        Module::pose q;
-        Calib::calibration(Now_calib, q, 0);  //qに対象画像のカメラ視点，位置が入る
-
-        cv::Mat R_calib = q.orientation * p.orientation.inv();
-        cv::Rodrigues(R_calib, rvec);
-
-
-        //offset world coordinate
-        cv::Mat offset = (cv::Mat_<double>(3, 1) << -337., -73.2, -410.);
-
-        //横軸: メータからの距離(chess boardからではない)
-        cv::Mat distance = -q.orientation.inv() * q.position + offset;
-
-        std::cout << "from meter: " << distance << std::endl;
-
-
-        cv::Mat calib_pos = p.orientation.inv() * p.position - q.orientation.inv() * q.position;
-        //cv::Mat calib_pos = p.orientation.inv() * p.position - q.orientation.inv() * q.position;
-
-        std::cout << "chessboard並進ベクトル" << std::endl
-                  << calib_pos << std::endl
-                  << "回転ベクトル" << std::endl
-                  << rvec << std::endl
-                  << std::endl;
-
-        //Homgraphy Estimation
-
-
-        Init::Feature ROI("../pictures/dia_experiment_V/roi/pic" + std::to_string(i) + ".png");
-        cv::Mat roi = cv::imread("../pictures/dia_experiment_V/roi/pic" + std::to_string(i) + ".png", 1);
-        double temp_size = 784.;
-        double rate = std::max(temp_size / roi.cols, temp_size / roi.rows);
-
-        cv::Mat resized_roi;
-        cv::resize(roi, resized_roi, cv::Size(), rate, rate);
-        Init::Feature R_ROI(resized_roi);
-
-        Base.getHomography(R_ROI);
-        for (auto j : R_ROI.featurePoint) {
-            ROI.featurePoint.push_back(cv::Point(j.x / rate + ROI_tl[i].x, j.y / rate + ROI_tl[i].y));  //resized_roi内座標をNow_clock内座標に変換
-        }
-
-        cv::Mat masks;
-        cv::Mat H4 = cv::findHomography(Base.featurePoint, ROI.featurePoint, masks, cv::RANSAC, 3);
-        cv::Mat warped_2 = cv::Mat::zeros(Now_clock.img.rows, Now_clock.img.cols, CV_8UC3);
-        cv::warpPerspective(Now_clock.img, warped_2, H4.inv(), warped_2.size());
-
-        Init::Feature Warp(warped_2);
-        cv::Mat H3 = Base.getHomography(Warp);
-
-
-        Module::pose r = Module::decomposeHomography(H4 * H3, camera.A);
-
-        std::cout << std::endl
-                  << "Homography分解で得られた並進ベクトル(world)" << std::endl
-                  << r.position << std::endl
-                  << std::endl
-                  << "回転ベクトル" << std::endl
-                  << r.orientation << std::endl
-                  << std::endl;
-
-        cv::Mat R_estimated;
-        cv::Rodrigues(r.orientation, R_estimated);
-
-        cv::Mat angle_error_mat = R_estimated * R_calib.inv();
-        cv::Mat angle_error;
-        cv::Rodrigues(angle_error_mat, angle_error);
-        double error = cv::norm(angle_error);
-
-        std::cout << "distance to meter (mm) = " << cv::norm(distance) << std::endl
-                  << "error dist(mm) = " << cv::norm(calib_pos - r.position) << std::endl
-                  << cv::norm(r.orientation) * 180.0 / CV_PI << ' ' << cv::norm(rvec) * 180.0 / CV_PI << ' ' << error * 180.0 / CV_PI << std::endl;
-
-        //if (cv::norm(calib_pos) > cv::norm(calib_pos - r.position)) {
-        //            ofs << i << ',' << r.position << std::endl;
-        ofs << i << ',' << cv::norm(distance) << ',' << cv::norm(calib_pos - r.position) << ',' << cv::norm(calib_pos - r.position) / cv::norm(calib_pos) << ',' << cv::norm(r.orientation) * 180.0 / CV_PI << ',' << cv::norm(rvec) * 180.0 / CV_PI << ',' << error * 180.0 / CV_PI << ',' << std::endl;
-        //}
-    }
-    return 0;
+    ofs << cv::norm(calib_pos - r.position) << ','
+        << cv::norm(calib_pos - r.position) / cv::norm(distance) << ','
+        << cv::norm(r.orientation) * 180.0 / CV_PI << ','
+        << error * 180.0 / CV_PI << ',' << std::endl;
+  }
+  return 0;
 }
